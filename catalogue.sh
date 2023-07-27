8be2854687c96854e51ee27337b9f52204841e3c
krishna <krishna.rayanapati@gmail.com>
2023-07-28 00:42:32 +0530

    Roboshop Scripts Update DNS Records

--- catalogue.sh
+++ catalogue.sh
@@ -1,19 +1,39 @@
+echo ">>>>>> Create Catalouge Service <<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo ">>>>>> Create MongoDB Repo <<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo ">>>>>> Install NodeJS Repos <<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo ">>>>>> Install NodeJS <<<<<<"
 yum install nodejs -y
+
+echo ">>>>>> Create Application User <<<<<<"
 useradd roboshop
+
+echo ">>>>>> Create Application Directory <<<<<<"
 mkdir /app
+
+echo ">>>>>> Download Application Content <<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>> Extract Application Content <<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+
+echo ">>>>>> Download NodeJS Dependencies <<<<<<"
 npm install
 
+echo ">>>>>> Install MongoDB Client <<<<<<"
 yum install mongodb-org-shell -y
+
+echo ">>>>>> Load Catalouge Schema <<<<<<"
 mongo --host mongodb.kdevops72.online </app/schema/catalogue.js
 
+echo ">>>>>> Start Catalouge Service <<<<<<"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
