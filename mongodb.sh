84b8b94e4be4569e9375c951e70e34070aeeeb7d
krishna <krishna.rayanapati@gmail.com>
2023-07-27 22:06:05 +0530

    Roboshop Scripts Update DNS Records

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 #update listen address 127.0.0.1 to 0.0.0.0
+systemctl daemon-reload
 systemctl enable mongod
 systemctl restart mongod
