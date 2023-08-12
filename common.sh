f9fda7eda06b44ea0e1619b639aacc41eb375cf2
krishna <krishna.rayanapati@gmail.com>
2023-08-12 13:04:35 +0530

    exit status

--- common.sh
+++ common.sh
@@ -1,31 +1,39 @@
 log=/tmp/roboshop.log
 
+func_exit_status(){
+    if [ $? -eq 0 ]; then
+      echo -e "\e[32m SUCCESS \e[0m"
+    else
+      echo -e "\e[31m FAILURE \e[0m"
+    fi
+}
+
 func_apppreq(){
 
   echo -e "\e[36m >>>>>> Create ${component} Service  <<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Create Application User <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   useradd roboshop &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Clean Existing Application Content <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   rm -rf /app &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Create Application Directory <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   mkdir /app &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Download Application Content <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Extract Application Content <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   cd /app
   unzip /tmp/${component}.zip &>>${log}
-  echo $?
+  func_exit_status
 }
 
 func_systemd() {
@@ -33,28 +41,28 @@ func_systemd() {
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
-    echo $?
+    func_exit_status
 }
 
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
     echo -e "\e[36m >>>>>> Install MongoDB Client <<<<<<\e[0m" | tee -a /tmp/roboshop.log
     yum install mongodb-org-shell -y &>>${log}
-    echo $?
+    func_exit_status
 
     echo -e "\e[36m >>>>>> Load Schema <<<<<<\e[0m" | tee -a /tmp/roboshop.log
     mongo --host mongodb.kdevops72.online </app/schema/${component}.js &>>${log}
-    echo $?
+    func_exit_status
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
       echo -e "\e[36m >>>>>> Install MySQL Client  <<<<<<\e[0m"
       yum install mysql -y &>>${log}
-      echo $?
+      func_exit_status
 
       echo -e "\e[36m >>>>>> Load Schema  <<<<<<\e[0m"
       mysql -h mysql.kdevops72.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
-      echo $?
+      func_exit_status
   fi
 }
 
@@ -63,21 +71,21 @@ func_nodejs() {
 
   echo -e "\e[36m >>>>>> Create MongoDB Repo <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Install NodeJS Repos <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m >>>>>> Install NodeJS <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   yum install nodejs -y &>>${log}
-  echo $?
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[36m >>>>>> Download NodeJS Dependencies <<<<<<\e[0m" | tee -a /tmp/roboshop.log
   npm install &>>${log}
-  echo $?
+  func_exit_status
 
   func_schema_setup
 
